b1927e64cc0b9331e851f27e3f18dec7f833b07a
AndriuBalashenko <andriu04081992@gmail.com>
2022-11-09 22:07:39 +0300

    We repeat the basic constructions of the Python language

---  a course for beginners/11.4.2_step_1.py
+++
@@ -1,9 +0,0 @@
-# Дополните приведенный код, так чтобы он вывел
-# сумму квадратов элементов списка numbers.
-
-numbers = [1, 78, 23, -65, 99, 9089, 34, -32, 0, -67, 1, 11, 111]
-sum_squares = 0
-for num in numbers:
-    num **= 2
-    sum_squares += num
-print(sum_squares)
