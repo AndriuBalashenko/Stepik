a0884af85e7f959c9c5777d892d1e80390746e6b
AndriuBalashenko <andriu04081992@gmail.com>
2022-11-23 21:48:30 +0300

    Создание таблицы новой

--- SQL/SQL_TEACH_Step_2.sql
+++ SQL/SQL_TEACH_Step_2.sql
@@ -241,3 +241,22 @@ SELECT author, title, amount,
 	IF (amount > 10, ABS(10 - amount), "Достаточно") AS Продать
 FROM book
 WHERE amount <> (10);
+
+/*
+Создать таблицу поставка (supply), которая имеет ту же структуру, что и таблиц book.
+
+Поле	Тип, описание
+supply_id	INT PRIMARY KEY AUTO_INCREMENT
+title	VARCHAR(50)
+author	VARCHAR(30)
+price	DECIMAL(8, 2)
+amount	INT
+*/
+
+CREATE TABLE supply(
+	supply_id INT PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(50),
+	author VARCHAR(30),
+	price DECIMAL(8, 2),
+	amount INT
+	);
