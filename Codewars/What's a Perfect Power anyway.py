bee7440ba9b568887e010bfb423834c9a80dfca9
Andriu Balashenko <andriu04081992@gmail.com>
2024-02-15 13:50:46 +0300

    work

--- Codewars/What's a Perfect Power anyway.py
+++ Codewars/What's a Perfect Power anyway.py
@@ -1,14 +1,9 @@
 def isPP(n):
-    r = list()
-    for i in range(1, n // 2 + 1):
-        for j in range(1, n // 2 + 1):
-            k = i ** j
-            if i ** j == n:
-                r.append(i)
-                r.append(j)
-                return r
-            elif i ** j > n:
-                break
-    return None
 
-print(isPP(4))
+    for i in range(2, n // 2 + 1):
+        for j in range(2, n // 2 + 1):
+            if i ** j > n:
+                break
+            elif i ** j == n:
+                return [i, j]
+    return None
